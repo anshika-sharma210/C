8f86a00403c6b51e289e56909c74c97a5ad27f80
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2020-10-10 03:27:01 -0700

    Add files via upload

---
+++ difference between two num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+
+int num1,num2,sub;
+
+printf("Enter Two Numbers:\n");
+scanf("%d %d",&num1,num2);
+sub=num1-num2;
+	
+	printf("\nDifference between %d and %d is =%d" ,num1,num2,sub);
+	return 0;
+}
+	
+
