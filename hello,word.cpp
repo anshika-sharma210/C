9e9e658a20a49baa975ae6c475ec271f6ed74907
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2020-10-10 03:25:46 -0700

    Add files via upload

---
+++ hello,word.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	//printf() display the string
+	printf("Hello,word!");
+	return 0;
+}
---
+++ sum of two num.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1,num2;
+	int ans;
+	num1=10;
+	num2=20;
+	 
+	 printf("num =%d \n",num1);
+	 printf("num =%d \n",num2);
+	 
+	 ans=num+num2;
+	 printf(ans =%d,ans);
+	 return0;
+}
