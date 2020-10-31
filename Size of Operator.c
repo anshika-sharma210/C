874f05d7bcd3ba46303ce788688347e5b6e912b3
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2020-10-30 20:05:33 -0700

    Add files via upload

---
+++ Size of Operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+	printf("Size of int=%1u bytes\n",sizeof(a));
+	printf("Size of float=%1u bytes\n",sizeof(b));
+	printf("Size of double=%1u bytes\n",sizeof(c));
+	printf("Size of char=%1u bytes\n",sizeof(d));
+	
+	return 0 ;
+} 
