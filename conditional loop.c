5572472a37acf38811307e8e113d85d776e41b07
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2020-11-05 07:58:06 -0800

    Add files via upload

--- conditional loop.c
+++ conditional loop.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main()
+
 {
-	int x;
-	/* The loop goes while x < 10, and increases by one evry loop*/
-	for (x =0; x < 10; x++ ) {
-		/* Keep in mind that the loop condition checks the conditional statement before it loops again. consequently, when x equals  10 the loop breaks. xis updated before the condition  is checked. */
-		printf( "%d\n" , x );
-	}
-	getchar();
+	int age;  //  variable declaration
+	printf("Enter your age");
+	
+	scanf("%d", &age); //taking user input for age variable 
+	(age >= 18)?(printf("eligible for voting")):(printf("not eligible for voting"));// conditional operator
+	
+	return 0;
 }
 
