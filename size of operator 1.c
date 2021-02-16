7019674fc686a1f0791c0cb5e09bc87ab054c031
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2021-02-16 09:47:06 -0800

    Add files via upload

---
+++ size of operator 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	char b;
+	float c;
+	double d;
+	printf("storage size for int data type:%d\n",sizeof(a));
+	printf("storage size for char data type:%d\n",sizeof(b));
+	printf("storage size for float data type:%d\n",sizeof(c));
+printf("storage size for int double type:%d\n",sizeof(d));
+return 0;
+}
+
